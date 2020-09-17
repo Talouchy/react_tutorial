0d138701429d0ca11939ace926cafb9885618ec2
Talouchy <danyal.lakzian@gmail.com>
2020-09-17 23:14:57 +1000

    hey

--- src/components/SingUpPage.jsx
+++ src/components/SingUpPage.jsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Container, Header, Content, Footer, IconButton, Icon } from "rsuite";
 
-
-function SingUpComp({toggleForm}) {
-
-    return(
-        <h3 onClick={toggleForm}>Login Page</h3>
-    );
-
+function SingUpComp({ toggleForm }) {
+  return <h3 onClick={toggleForm}>Pooyan is OG</h3>;
 }
 
 export default SingUpComp;
